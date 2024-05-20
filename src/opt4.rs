f58e63280d0a2dd356b29751e0d606c661900f06
Thomas Ip <thomas@ipthomas.com>
2024-05-20 13:08:18 +0100

    Replace HashMap hasher with custom FxHash

--- src/opt4.rs
+++ src/opt4.rs
@@ -1,5 +1,13 @@
 use core::slice;
-use std::{collections::HashMap, fs::File, ops::BitXor, sync::Mutex, thread};
+use std::{
+    collections::HashMap,
+    fs::File,
+    hash::{BuildHasher, Hasher},
+    ops::BitXor,
+    sync::{atomic::AtomicUsize, Mutex},
+    thread,
+    time::Instant,
+};
 
 use memmap2::Mmap;
 use metal::{objc::rc::autoreleasepool, Device, MTLSize};
@@ -9,60 +17,166 @@ use crate::{
     U32_SIZE,
 };
 
+type StationsHashMap<'a> = HashMap<&'a str, Station, BuildFxHash>;
+
 const GPU_OFFLOAD_RATIO: f32 = 0.5;
 const CHUNK_LEN: u64 = {
     let len = 2 * 1024;
     assert!(len % 16 == 0);
     len
 };
-const HASHMAP_LEN: u64 = 512;
-const HASHMAP_RAW_LEN: usize = HASHMAP_LEN as usize * 4;
+const HASHMAP_LEN: usize = 512;
+const HASHMAP_RAW_LEN: usize = HASHMAP_LEN * 4;
 
-#[repr(align(32))]
-struct MPHStationNames {
-    /// 32-byte aligned station names sorted using the perfect minimal hash index
-    keys: [u8; 512 * 32],
-    /// Map position in `keys` to index in the alphabetically sorted station name array
-    indices: [usize; 512],
+/// Slightly modified FxHash (from Rustc & Firefox)
+///
+/// Does not break down trailing bytes into 4, 2, 1 bytes
+/// chunks unlike the original implementation.
+struct FxHash {
+    hash: u64,
 }
 
-impl MPHStationNames {
+impl FxHash {
+    const K: u64 = 0x517cc1b727220a95;
+
+    fn hash64(&mut self, x: u64) {
+        self.hash = self.hash.rotate_left(5).bitxor(x).wrapping_mul(Self::K);
+    }
+
+    fn hash(s: &str) -> u64 {
+        let mut h = FxHash { hash: 0 };
+        h.write(s.as_bytes());
+        h.finish()
+    }
+}
+
+impl Hasher for FxHash {
+    fn finish(&self) -> u64 {
+        self.hash
+    }
+
+    fn write(&mut self, mut bytes: &[u8]) {
+        while bytes.len() >= 8 {
+            let chunk = u64::from_le_bytes(bytes[..8].try_into().unwrap());
+            self.hash64(chunk);
+            bytes = &bytes[8..];
+        }
+        if !bytes.is_empty() {
+            let mut buf = [0u8; 8];
+            buf[..bytes.len()].copy_from_slice(bytes);
+            self.hash64(u64::from_le_bytes(buf));
+        }
+    }
+}
+
+struct BuildFxHash;
+
+impl BuildHasher for BuildFxHash {
+    type Hasher = FxHash;
+
+    fn build_hasher(&self) -> Self::Hasher {
+        FxHash { hash: 0 }
+    }
+}
+
+struct MPHStations<'a> {
+    buckets: [i32; HASHMAP_RAW_LEN],
+    fallback: StationsHashMap<'a>,
+}
+
+impl<'a> MPHStations<'a> {
+    fn new() -> Self {
+        Self {
+            buckets: std::array::from_fn(|i| match i % 4 {
+                0 => i32::MAX,
+                1 => i32::MIN,
+                _ => 0,
+            }),
+            fallback: StationsHashMap::with_hasher(BuildFxHash),
+        }
+    }
+
+    fn merge(&mut self, other: MPHStations<'a>) {
+        for (bucket, other_bucket) in self
+            .buckets
+            .chunks_exact_mut(4)
+            .zip(other.buckets.chunks_exact(4))
+        {
+            if other_bucket[0] < bucket[0] {
+                bucket[0] = other_bucket[0];
+            }
+            if other_bucket[1] > bucket[1] {
+                bucket[1] = other_bucket[1];
+            }
+            bucket[2] += other_bucket[2];
+            bucket[3] += other_bucket[3];
+        }
+        for (name, other_station) in other.fallback.into_iter() {
+            if let Some(station) = self.fallback.get_mut(name) {
+                station.merge(&other_station);
+            } else {
+                self.fallback.insert(name, other_station);
+            }
+        }
+    }
+
+    fn to_stations(mut self, params: &MPHParams) -> Stations<'a> {
+        for (i, name) in STATION_NAMES.into_iter().enumerate() {
+            let offset = params.indices[i] * 4;
+            let min = self.buckets[offset];
+            let max = self.buckets[offset + 1];
+            let sum = self.buckets[offset + 2];
+            let count = self.buckets[offset + 3];
+            if let Some(station) = self.fallback.get_mut(name) {
+                station.min = station.min.min(min);
+                station.max = station.max.max(max);
+                station.sum += sum;
+                station.count += count;
+            } else {
+                self.fallback.insert(
+                    name,
+                    Station {
+                        min,
+                        max,
+                        sum,
+                        count,
+                    },
+                );
+            }
+        }
+        Stations {
+            inner: self.fallback.into_iter().collect(),
+        }
+    }
+}
+
+#[repr(align(32))]
+struct MPHParams {
+    /// 32-byte aligned station names sorted using the perfect minimal hash index
+    keys: [u8; HASHMAP_LEN * 32],
+    /// Map station names to keys index
+    indices: [usize; STATION_NAMES.len()],
+}
+
+impl MPHParams {
     fn init() -> Self {
-        let mut this = MPHStationNames {
-            keys: [0; 512 * 32],
-            indices: [0; 512],
+        let mut this = MPHParams {
+            keys: [0; HASHMAP_LEN * 32],
+            indices: [0; STATION_NAMES.len()],
         };
         for (i, name) in STATION_NAMES.iter().enumerate() {
-            let idx = Self::mph_index(name);
-            this.indices[idx] = i;
+            let idx = Self::index(name);
+            this.indices[i] = idx;
             this.keys[idx * 32..idx * 32 + name.len()].copy_from_slice(name.as_bytes());
         }
         this
-    }
-
-    /// Slightly modified FxHash (from Rustc & Firefox)
-    ///
-    /// Does not break down trailing bytes into 4, 2, 1 bytes
-    /// chunks unlike the original implementation.
-    fn fxhash(s: &str) -> u64 {
-        const K: u64 = 0x517cc1b727220a95;
-
-        let mut h: u64 = 0;
-        for chunk in s.as_bytes().chunks(8) {
-            let mut buf = [0u8; 8];
-            buf[..chunk.len()].copy_from_slice(chunk);
-            let i = u64::from_le_bytes(buf);
-            h = h.rotate_left(5).bitxor(i).wrapping_mul(K);
-        }
-        h
     }
 
     /// Minimal perfect hash index of station names
     ///
     /// Constructed with PTRHash (https://github.com/RagnarGrootKoerkamp/PTRHash)
     /// Map all 314 station names uniquely to 512 buckets
-    fn mph_index(name: &str) -> usize {
-        const C: u64 = 0x517cc1b727220a95;
+    fn index(name: &str) -> usize {
         const PILOTS: [u8; 78] = [
             50, 110, 71, 13, 18, 27, 21, 14, 10, 16, 1, 14, 6, 11, 0, 2, 17, 2, 1, 4, 68, 79, 21,
             0, 22, 20, 60, 12, 30, 53, 62, 78, 27, 17, 2, 17, 13, 43, 21, 108, 19, 12, 25, 1, 55,
@@ -74,17 +188,18 @@ impl MPHStationNames {
         const C3: isize = -55;
         const P1: u64 = 11068046444225730560;
 
-        let hash = Self::fxhash(name);
+        let hash = FxHash::hash(name);
         let is_large = hash >= P1;
         let rem = if is_large { REM_C2 } else { REM_C1 };
         let bucket = (is_large as isize * C3) + ((hash as u128 * rem as u128) >> 64) as isize;
         let pilot = PILOTS[bucket as usize];
-        return ((C as u128 * (hash ^ C.wrapping_mul(pilot as u64)) as u128) >> 64) as usize
+        return ((FxHash::K as u128 * (hash ^ FxHash::K.wrapping_mul(pilot as u64)) as u128) >> 64)
+            as usize
             & ((1 << 9) - 1) as usize;
     }
 }
 
-fn cpu_impl<'a>(buf: &'a [u8], stations: &mut HashMap<&'a str, Station>) {
+fn cpu_impl<'a>(buf: &'a [u8], stations: &mut StationsHashMap<'a>) {
     buf[..buf.len().saturating_sub(1)]
         .split(is_newline)
         .filter(|line| !line.is_empty())
@@ -120,33 +235,6 @@ fn cpu_impl<'a>(buf: &'a [u8], stations: &mut HashMap<&'a str, Station>) {
         });
 }
 
-fn insert_gpu_hashmap(
-    buckets: &[i32; HASHMAP_RAW_LEN],
-    mph: &MPHStationNames,
-    stations: &mut HashMap<&str, Station>,
-) {
-    for (&sorted_idx, bucket) in mph.indices.iter().zip(buckets.chunks_exact(4)) {
-        let name = STATION_NAMES[sorted_idx];
-        let (min, max, sum, count) = (bucket[0], bucket[1], bucket[2], bucket[3]);
-        if let Some(station) = stations.get_mut(name) {
-            station.min = station.min.min(min);
-            station.max = station.max.max(max);
-            station.sum += sum;
-            station.count += count;
-        } else {
-            stations.insert(
-                name,
-                Station {
-                    min,
-                    max,
-                    sum,
-                    count,
-                },
-            );
-        }
-    }
-}
-
 fn split_buf_once(buf: &[u8], max_len: usize) -> (&[u8], &[u8]) {
     if buf.len() > max_len {
         let mid = buf[..max_len].iter().rposition(|&c| c == b'\n').unwrap() + 1;
@@ -163,7 +251,7 @@ fn split_buf_once(buf: &[u8], max_len: usize) -> (&[u8], &[u8]) {
 /// (~13GB). To work around this issue we split the file into chunks aligned to
 /// line boundaries.
 fn split_buf(mut buf: &[u8], max_len: usize) -> Vec<&[u8]> {
-    let mut bufs = Vec::with_capacity(buf.len().div_ceil(max_len));
+    let mut bufs = vec![];
     while !buf.is_empty() {
         let (head, tail) = split_buf_once(buf, max_len);
         buf = tail;
@@ -214,23 +302,18 @@ fn chop_head_and_tail<'a, const CHUNK_LEN: usize>(buf: &'a [u8]) -> ChopInfo<'a>
     }
 }
 
-fn process_gpu<'a, F>(buf: &'a [u8], kernel_getter: F) -> Stations<'a>
+fn process_gpu<'a, F>(buf: &'a [u8], mph_params: &MPHParams, kernel_getter: F) -> MPHStations<'a>
 where
     F: FnOnce(&metal::Device) -> metal::Function,
 {
+    let start = Instant::now();
     let split_bufs = split_buf(&buf, u32::MAX as usize);
     let chop_infos = split_bufs
         .iter()
         .map(|buf| chop_head_and_tail::<{ CHUNK_LEN as usize }>(buf))
         .collect::<Vec<_>>();
 
-    let buckets: [i32; HASHMAP_RAW_LEN] = std::array::from_fn(|i| match i % 4 {
-        0 => i32::MAX,
-        1 => i32::MIN,
-        _ => 0,
-    });
-    let mut stations = Stations::default();
-    let mph = MPHStationNames::init();
+    let mut stations = MPHStations::new();
 
     let fallback = [false];
 
@@ -250,8 +333,8 @@ where
             .new_compute_pipeline_state_with_function(&kernel)
             .unwrap();
 
-        let device_buckets = device_buffer(device, &buckets);
-        let device_mph_keys = device_buffer(device, &mph.keys);
+        let device_buckets = device_buffer(device, &stations.buckets);
+        let device_mph_keys = device_buffer(device, &mph_params.keys);
         let device_fallback = device_buffer(device, &fallback);
 
         for (
@@ -283,8 +366,8 @@ where
         cmd_buf.commit();
 
         for ChopInfo { head, tail, .. } in chop_infos {
-            cpu_impl(head, &mut stations.inner);
-            cpu_impl(tail, &mut stations.inner);
+            cpu_impl(head, &mut stations.fallback);
+            cpu_impl(tail, &mut stations.fallback);
         }
 
         cmd_buf.wait_until_completed();
@@ -292,26 +375,25 @@ where
 
     assert!(!fallback[0], "Need fallback");
 
-    insert_gpu_hashmap(&buckets, &mph, &mut stations.inner);
+    eprintln!(
+        "process_gpu elapsed={:.2}ms buf={}MB",
+        start.elapsed().as_secs_f64() * 1000.,
+        buf.len() / (1024 * 1024)
+    );
 
     stations
 }
 
-fn process_cpu<'a>(buf: &'a [u8]) -> Stations<'a> {
-    let mut stations = Stations::default();
-    cpu_impl(buf, &mut stations.inner);
+fn process_cpu<'a>(buf: &'a [u8], mph_params: &MPHParams, offset: &AtomicUsize) -> MPHStations<'a> {
+    let mut stations = MPHStations::new();
+    let start = Instant::now();
+    cpu_impl(buf, &mut stations.fallback);
+    eprintln!(
+        "process_cpu elapsed={:.2}ms buf={}",
+        start.elapsed().as_secs_f64() * 1000.,
+        buf.len()
+    );
     stations
-}
-
-fn merge_stations<'a>(base: &Mutex<Stations<'a>>, update: Stations<'a>) {
-    let base = &mut base.lock().unwrap().inner;
-    for (name, station) in update.inner.into_iter() {
-        if let Some(base_station) = base.get_mut(name) {
-            base_station.merge(&station);
-        } else {
-            base.insert(name, station);
-        }
-    }
 }
 
 pub fn process<'a, F: Send>(file: &'a File, kernel_getter: F)
@@ -320,19 +402,30 @@ where
 {
     let buf = unsafe { &Mmap::map(file).unwrap() };
     let (gpu_buf, cpu_buf) = split_buf_once(buf, (buf.len() as f32 * GPU_OFFLOAD_RATIO) as usize);
-    // let (gpu_buf, cpu_buf) = split_buf_once(buf, buf.len());
 
-    let n_cpus = thread::available_parallelism().unwrap().get();
+    let n_cpus = thread::available_parallelism().unwrap().get() - 1;
     let cpu_chunk_size = cpu_buf.len().div_ceil(n_cpus);
     let cpu_chunks = split_buf(cpu_buf, cpu_chunk_size);
+    let cpu_buf_offset = AtomicUsize::new(0);
 
-    let stations = Mutex::new(Stations::default());
+    let stations = Mutex::new(MPHStations::new());
+    let mph_params = MPHParams::init();
+
     thread::scope(|s| {
-        s.spawn(|| merge_stations(&stations, process_gpu(gpu_buf, kernel_getter)));
+        s.spawn(|| {
+            let local = process_gpu(gpu_buf, &mph_params, kernel_getter);
+            stations.lock().unwrap().merge(local);
+        });
         for chunk in cpu_chunks {
-            s.spawn(|| merge_stations(&stations, process_cpu(chunk)));
+            s.spawn(|| {
+                let local = process_cpu(chunk, &mph_params, &cpu_buf_offset);
+                stations.lock().unwrap().merge(local);
+            });
         }
     });
 
-    println!("{}", stations.into_inner().unwrap());
+    println!(
+        "{}",
+        stations.into_inner().unwrap().to_stations(&mph_params)
+    );
 }
