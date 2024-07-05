36efa018473f1f8c48fccb23bc6e4c095eecbb51
Thomas Ip <thomas@ipthomas.com>
2024-07-05 13:40:41 +0100

    Implement FCH minimal perfect hash function

---
+++ src/bin/mphf.rs
@@ -0,0 +1,120 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+
+use one_billion_row::STATION_NAMES;
+
+#[derive(Debug)]
+struct Fch {
+    n: u64,
+    m: u64,
+    p1: u64,
+    p2: u64,
+    s2: u64,
+    ds: Vec<u64>,
+}
+
+impl Fch {
+    const S1: u64 = 0;
+
+    fn construct(keys: &[&str], c: f64) -> Self {
+        let n = keys.len() as f64;
+        let m = ((c * n) / (n.log2() + 1.)).ceil();
+        let p1 = (0.6 * n) as u64;
+        let p2 = (0.3 * m) as u64;
+        let n = keys.len() as u64;
+        let m = m as u64;
+
+        // 1. Mapping
+        let mut buckets = (0..m as usize).map(|i| (i, vec![])).collect::<Vec<_>>();
+        for &key in keys {
+            let h = Self::hash(key, Self::S1);
+            let bucket_idx = if (h % n) < p1 {
+                h % p2 // S1
+            } else {
+                p2 + (h % (m - p2)) // S2
+            } as usize;
+            buckets[bucket_idx].1.push(key);
+        }
+
+        // 2. Ordering
+        buckets.sort_by_key(|(_, bucket)| -(bucket.len() as i64));
+
+        // 3. Searching
+        let mut s2 = 0;
+        let ds = 's: loop {
+            let mut occupied = vec![false; keys.len()];
+            let mut ds = vec![u64::MAX; buckets.len()];
+            'bucket: for &(bucket_idx, ref bucket) in &buckets {
+                for d in 0.. {
+                    let mut pos_arr = bucket
+                        .iter()
+                        .map(|&key| ((Self::hash(key, s2) + d) % n) as usize)
+                        .collect::<Vec<_>>();
+                    if has_collision(&mut pos_arr) {
+                        s2 += 1;
+                        continue 's;
+                    }
+                    if pos_arr.iter().all(|&pos| !occupied[pos]) {
+                        pos_arr.into_iter().for_each(|pos| occupied[pos] = true);
+                        ds[bucket_idx] = d;
+                        continue 'bucket;
+                    }
+                }
+            }
+            break ds;
+        };
+
+        Self {
+            n,
+            m,
+            p1,
+            p2,
+            s2,
+            ds,
+        }
+    }
+
+    fn index(&self, key: &str) -> usize {
+        let h = Self::hash(key, Self::S1);
+        let bucket_idx = if (h % self.n) < self.p1 {
+            h % self.p2
+        } else {
+            self.p2 + (h % (self.m - self.p2))
+        };
+        let d = self.ds[bucket_idx as usize];
+        ((Self::hash(key, self.s2) + d as u64) % self.n) as usize
+    }
+
+    fn hash(key: &str, seed: u64) -> u64 {
+        let mut s = DefaultHasher::new();
+        seed.hash(&mut s);
+        key.hash(&mut s);
+        s.finish()
+    }
+}
+
+fn has_collision<T: PartialOrd + Ord>(arr: &mut Vec<T>) -> bool {
+    let n = arr.len();
+    arr.sort_unstable();
+    arr.dedup();
+    arr.len() < n
+}
+
+fn verify<F>(index_fn: F)
+where
+    F: Fn(&str) -> usize,
+{
+    let mut occupied = vec![false; STATION_NAMES.len()];
+    for name in STATION_NAMES {
+        let i = index_fn(name);
+        if occupied[i] {
+            println!("{} {}", name, i);
+        }
+        occupied[i] = true;
+    }
+}
+
+fn main() {
+    let fch = Fch::construct(&STATION_NAMES, 5.);
+    println!("{:?}", fch);
+    verify(|x| fch.index(x));
+}
